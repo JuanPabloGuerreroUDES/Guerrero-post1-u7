9d6e0088ba2d274fe39fa04d1e313c6a3a0cdf47
Juan Pablo Guerrero Hernandez <juan.guerro.22@gmail.com>
2026-04-20 08:49:14 -0500

    Commit 2 - Se agrega la estructura model, service, controller

--- src/main/java/com/universidad/productosweb/ProductosWebApplication.java
+++ src/main/java/com/universidad/productosweb/ProductosWebApplication.java
@@ -1,4 +1,4 @@
-package com.universidad.productos_web;
+package com.universidad.productosweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
